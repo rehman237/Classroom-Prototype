df4de0e1a7a75b0e34fd71b5a523536bc6d824da
rehman237 <rehmanmani196@gmail.com>
2023-12-07 17:29:44 +0500

    code of homepage

--- classroom-react-app/src/classroom/classroom.js
+++ classroom-react-app/src/classroom/classroom.js
@@ -1,8 +1,30 @@
 import React, { useState } from "react";
-import "../App.css";
+import "./classroom.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faComment,
+  faPhoneSlash,
+  faMicrophone,
+  faChromecast,
+  faPhone,
+  faVideo,
+  faBell,
+  faUser,
+  faCog,
+} from "@fortawesome/free-solid-svg-icons";
+
 import homeLogo from "../photos/home-logo.png";
 import teacher from "../photos/teacher.jpg";
-
+import people1Img from "../photos/people-1.png";
+import people2Img from "../photos/people-2.png";
+import people3Img from "../photos/people-3.png";
+import people4Img from "../photos/people-4.png";
+import people5Img from "../photos/people-5.png";
+import user1 from "../photos/user-1.png";
+import user2 from "../photos/user-2.png";
+import user3 from "../photos/user-3.png";
+import user4 from "../photos/user-4.png";
+import user5 from "../photos/user-5.png";
 function ClassRoom() {
   const handleCreateRoom = () => {
     // Add logic to handle creating a room
@@ -11,65 +33,62 @@ function ClassRoom() {
 
   return (
     <section className="main">
-      <div class="header">
+      <div className="header">
         <nav>
-          <img src={homeLogo} alt="homeLogo" class="logo" />
+          <img src={homeLogo} alt="homeLogo" className="logo" />
           <ul>
             <li>
-              <i class="fa-solid fa-phone" id="active"></i>
+              <FontAwesomeIcon icon={faPhone} id="active" />
             </li>
             <li>
-              <i class="fa-solid fa-video"></i>
+              <FontAwesomeIcon icon={faVideo} />
             </li>
             <li>
-              <i class="fa-solid fa-message"></i>
+              <FontAwesomeIcon icon={faBell} />
             </li>
             <li>
-              <i class="fa-solid fa-bell"></i>
+              <FontAwesomeIcon icon={faUser} />
             </li>
             <li>
-              <i class="fa-solid fa-user"></i>
-            </li>
-            <li>
-              <i class="fa-solid fa-gear"></i>
+              <FontAwesomeIcon icon={faCog} />
             </li>
           </ul>
         </nav>
-        <div class="container">
-          <div class="top-icons">
-            <i class="fa fa-search"></i>
-            <i class="fa-solid fa-bars"></i>
+        <div className="container">
+          <div className="top-icons">
+            <i className="fa fa-search"></i>
+            <i className="fa-solid fa-bars"></i>
           </div>
-          <div class="row">
-            <div class="col-1">
-              <img src={teacher} class="host-img" />
-              <div class="controls">
-                <i class="far fa-comment"></i>
-                <i class="fas fa-phone-slash"></i>
-                <i class="fa fa-phone"></i>
-                <i class="fa-solid fa-microphone"></i>
-                <i class="fa-brands fa-chromecast"></i>
+          <div className="row">
+            <div className="col-1">
+              <img src={teacher} className="host-img" />
+              <div className="controls">
+                <FontAwesomeIcon icon={faComment} />
+                <FontAwesomeIcon icon={faPhoneSlash} />
+                <FontAwesomeIcon icon={faPhone} />
+                <FontAwesomeIcon icon={faMicrophone} />
+                <FontAwesomeIcon icon={faChromecast} />
               </div>
             </div>
-            <div class="col-2">
-              <div class="joined">
+            <div className="col-2">
+              <div className="joined">
                 <p>People joined</p>
                 <div>
-                  <img src="people-1.png" />
-                  <img src="people-2.png" />
-                  <img src="people-3.png" />
-                  <img src="people-4.png" />
-                  <img src="people-5.png" />
+                  <img src={people1Img} alt="People 1" />
+                  <img src={people2Img} alt="Person 2" />
+                  <img src={people3Img} alt="Person 3" />
+                  <img src={people4Img} alt="Person 4" />
+                  <img src={people5Img} alt="Person 5" />
                 </div>
               </div>
-              <div class="invite">
+              <div className="invite">
                 <p>Invite more people</p>
                 <div>
-                  <img src="user-1.png" />
-                  <img src="user-2.png" />
-                  <img src="user-3.png" />
-                  <img src="user-4.png" />
-                  <img src="user-5.png" />
+                  <img src={user1} alt="User 1" />
+                  <img src={user2} alt="User 2" />
+                  <img src={user3} alt="User 3" />
+                  <img src={user4} alt="User 4" />
+                  <img src={user5} alt="User 5" />
                 </div>
               </div>
             </div>
